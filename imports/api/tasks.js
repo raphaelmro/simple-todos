671e441b3338236bde5909c53906a4383709fa6e
Raphael Onofre <raphaelmoura@codata.pb.gov.br>
2016-09-19 16:36:36 -0300

    adding methods privacy

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -3,6 +3,14 @@ import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isServer) {
+  //This code only runs on the server
+  Meteor.publish('tasks', function taskPublication(){
+    return Tasks.find();
+  });
+}
+
 Meteor.methods({
   'tasks.insert'(text){
     check(text, String);
@@ -28,8 +36,19 @@ Meteor.methods({
 
     Tasks.update(taskId,{$set: {checked: setChecked}});
   },
+  'tasks.setPrivate'(taskId, setToPrivate){
+    check(taskId, String);
+    check(setToPrivate, Boolean);
 
+    const task = Tasks.findOne(taskId);
 
+    // Make sure only the task owner can make private
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
 
+    Tasks.update(taskId, {$set: {private: setToPrivate}});
+
+  },
 
 })
